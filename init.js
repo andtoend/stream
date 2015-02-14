92ecda653e716fe7fbac25ed1bc55eae2ed5939a
javier <salomeka4@gmail.com>
2015-02-13 21:56:42 -0500

    correcion

--- init.js
+++ init.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname +'/index.html');
 });
 
-http.listen(3000, function(){
+http.listen(8080, function(){
   console.log('listening on *:3000');
 });
 
